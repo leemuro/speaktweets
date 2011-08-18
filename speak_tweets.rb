733cd5d851b7e91f32ad952115c0dbe45c63b1f1
Lee Muro <lee@leemuro.com>
2011-08-18 19:14:27 -0400

    now loops and speaks all new tweets

--- speak_tweets.rb
+++ speak_tweets.rb
@@ -1,9 +1,16 @@
 require 'rubygems'
 require 'twitter'
 
-tweets = Twitter::Search.new.hashtag("burningriverdevs").no_retweets.fetch
+last_max_id = 0
 
-tweets.each do |tweet|
-    puts tweet[:text]
-    system "say \"#{tweet[:text]}\""
+while true
+    tweets = Twitter::Search.new.hashtag("burningriverdevs").no_retweets.fetch
+
+    tweets.each do |tweet|
+        if tweet.id > last_max_id
+            last_max_id = tweet.id
+            puts tweet[:text]
+            system "say \"#{tweet[:text]}\""
+        end
+    end
 end
